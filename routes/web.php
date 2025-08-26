81e455926f49a111a26ab77cbad549e066d1ac21
Habibur Rahaman <shamolrahaman@gmail.com>
2025-08-27 02:44:21 +0600

    Fix live search route override

--- routes/web.php
+++ routes/web.php
@@ -27,8 +27,8 @@ Route::group(['prefix' => '/dashboard', 'middleware' => 'auth'], function () {
     // profile
     Route::resource('profile', ProfileController::class);
 
-    Route::resource('nu-smart-card', DashboardController::class);
     Route::get('nu-smart-card/live-search', [DashboardController::class, 'liveSearch'])->name('nu-smart-card.live-search');
+    Route::resource('nu-smart-card', DashboardController::class);
     Route::get('view-pdf', [DashboardController::class, 'getPdfData'])->name('view-pdf');
     Route::get('single-pdf/{id}', [DashboardController::class, 'getSinglePdfData'])->name('single-pdf');
     Route::get('export-word', [DashboardController::class, 'exportWord'])->name('export-word');

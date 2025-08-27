1d04575dc546d3bedeb733ecd3671a608bc652c5
creativehabib <shamolrahaman@gmail.com>
2025-08-28 02:16:44 +0600

    update

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -25,7 +25,6 @@
                               <iconify-icon icon="mingcute:home-3-line"></iconify-icon>
                          </span>
                          <span class="nav-text"> Dashboard </span>
-                         <span class="badge bg-primary badge-pill text-end">03</span>
                     </a>
                </li>
               <li class="nav-item">
@@ -40,9 +39,6 @@
                       <ul class="nav sub-navbar-nav">
                           <li class="sub-nav-item">
                               <a class="sub-nav-link" href="{{ route ('nu-smart-card.index') }}">All List</a>
-                          </li>
-                          <li class="sub-nav-item">
-                              <a class="sub-nav-link" href="{{ route ('second' , ['auth','signup']) }}">Sign Up</a>
                           </li>
                           <li class="sub-nav-item">
                               <a class="sub-nav-link" href="{{ route ('blood-group.index') }}">Blood Group</a>
@@ -83,51 +79,6 @@
                        <span class="nav-text"> Settings </span>
                   </a>
               </li>
-              <li class="nav-item">
-                    <a class="nav-link menu-arrow" href="#sidebarAuthentication" data-bs-toggle="collapse" role="button"
-                         aria-expanded="false" aria-controls="sidebarAuthentication">
-                         <span class="nav-icon">
-                              <iconify-icon icon="mingcute:user-3-line"></iconify-icon>
-                         </span>
-                         <span class="nav-text"> Authentication </span>
-                    </a>
-                    <div class="collapse" id="sidebarAuthentication">
-                         <ul class="nav sub-navbar-nav">
-                              <li class="sub-nav-item">
-                                   <a class="sub-nav-link" href="{{ route ('second' , ['auth','signin']) }}">Sign In</a>
-                              </li>
-                              <li class="sub-nav-item">
-                                   <a class="sub-nav-link" href="{{ route ('second' , ['auth','signup']) }}">Sign Up</a>
-                              </li>
-                              <li class="sub-nav-item">
-                                   <a class="sub-nav-link" href="{{ route ('second' , ['auth','password']) }}">Reset Password</a>
-                              </li>
-                              <li class="sub-nav-item">
-                                   <a class="sub-nav-link" href="{{ route ('second' , ['auth','lock-screen']) }}">Lock Screen</a>
-                              </li>
-                         </ul>
-                    </div>
-               </li>
-
-               <li class="nav-item">
-                    <a class="nav-link menu-arrow" href="#sidebarError" data-bs-toggle="collapse" role="button"
-                         aria-expanded="false" aria-controls="sidebarError">
-                         <span class="nav-icon">
-                              <iconify-icon icon="mingcute:bug-line"></iconify-icon>
-                         </span>
-                         <span class="nav-text"> Error Pages</span>
-                    </a>
-                    <div class="collapse" id="sidebarError">
-                         <ul class="nav sub-navbar-nav">
-                              <li class="sub-nav-item">
-                                   <a class="sub-nav-link" href="{{ route ('second' , ['pages','404']) }}">Pages 404</a>
-                              </li>
-                              <li class="sub-nav-item">
-                                   <a class="sub-nav-link" href="{{ route ('second' , ['pages','404-alt']) }}">Pages 404 Alt</a>
-                              </li>
-                         </ul>
-                    </div>
-               </li>
 
                <li class="menu-title">UI Kit...</li>
 
@@ -209,104 +160,6 @@
                     </div>
                </li>
 
-               <li class="nav-item">
-                    <a class="nav-link" href="{{ route ('second' , ['pages','charts']) }}">
-                         <span class="nav-icon">
-                              <iconify-icon icon="mingcute:chart-bar-line"></iconify-icon>
-                         </span>
-                         <span class="nav-text"> Apex Charts </span>
-                    </a>
-               </li>
-
-               <li class="nav-item">
-                    <a class="nav-link menu-arrow" href="#sidebarForms" data-bs-toggle="collapse" role="button"
-                         aria-expanded="false" aria-controls="sidebarForms">
-                         <span class="nav-icon">
-                              <iconify-icon icon="mingcute:box-line"></iconify-icon>
-                         </span>
-                         <span class="nav-text"> Forms </span>
-                    </a>
-                    <div class="collapse" id="sidebarForms">
-                         <ul class="nav sub-navbar-nav">
-                              <li class="sub-nav-item">
-                                   <a class="sub-nav-link" href="{{ route ('second' , ['forms','basic']) }}">Basic Elements</a>
-                              </li>
-                              <li class="sub-nav-item">
-                                   <a class="sub-nav-link" href="{{ route ('second' , ['forms','flatpicker']) }}">Flatpicker</a>
-                              </li>
-                              <li class="sub-nav-item">
-                                   <a class="sub-nav-link" href="{{ route ('second' , ['forms','validation']) }}">Validation</a>
-                              </li>
-                              <li class="sub-nav-item">
-                                   <a class="sub-nav-link" href="{{ route ('second' , ['forms','fileuploads']) }}">File Upload</a>
-                              </li>
-                              <li class="sub-nav-item">
-                                   <a class="sub-nav-link" href="{{ route ('second' , ['forms','editors']) }}">Editors</a>
-                              </li>
-                         </ul>
-                    </div>
-               </li>
-
-               <li class="nav-item">
-                    <a class="nav-link menu-arrow" href="#sidebarTables" data-bs-toggle="collapse" role="button"
-                         aria-expanded="false" aria-controls="sidebarTables">
-                         <span class="nav-icon">
-                              <iconify-icon icon="mingcute:table-line"></iconify-icon>
-                         </span>
-                         <span class="nav-text"> Tables </span>
-                    </a>
-                    <div class="collapse" id="sidebarTables">
-                         <ul class="nav sub-navbar-nav">
-                              <li class="sub-nav-item">
-                                   <a class="sub-nav-link" href="{{ route ('second' , ['tables','basic']) }}">Basic Tables</a>
-                              </li>
-                              <li class="sub-nav-item">
-                                   <a class="sub-nav-link" href="{{ route ('second' , ['tables','gridjs']) }}">Grid Js</a>
-                              </li>
-                         </ul>
-                    </div>
-               </li>
-
-               <li class="nav-item">
-                    <a class="nav-link menu-arrow" href="#sidebarIcons" data-bs-toggle="collapse" role="button"
-                         aria-expanded="false" aria-controls="sidebarIcons">
-                         <span class="nav-icon">
-                              <iconify-icon icon="mingcute:dribbble-line"></iconify-icon>
-                         </span>
-                         <span class="nav-text"> Icons </span>
-                    </a>
-                    <div class="collapse" id="sidebarIcons">
-                         <ul class="nav sub-navbar-nav">
-                              <li class="sub-nav-item">
-                                   <a class="sub-nav-link" href="{{ route ('second' , ['icons','boxicons']) }}">Boxicons</a>
-                              </li>
-                              <li class="sub-nav-item">
-                                   <a class="sub-nav-link" href="{{ route ('second' , ['icons','solar']) }}">Solar Icons</a>
-                              </li>
-                         </ul>
-                    </div>
-               </li>
-
-               <li class="nav-item">
-                    <a class="nav-link menu-arrow" href="#sidebarMaps" data-bs-toggle="collapse" role="button"
-                         aria-expanded="false" aria-controls="sidebarMaps">
-                         <span class="nav-icon">
-                              <iconify-icon icon="mingcute:map-line"></iconify-icon>
-                         </span>
-                         <span class="nav-text"> Maps </span>
-                    </a>
-                    <div class="collapse" id="sidebarMaps">
-                         <ul class="nav sub-navbar-nav">
-                              <li class="sub-nav-item">
-                                   <a class="sub-nav-link" href="{{ route ('second' , ['maps','google']) }}">Google Maps</a>
-                              </li>
-                              <li class="sub-nav-item">
-                                   <a class="sub-nav-link" href="{{ route ('second' , ['maps','vector']) }}">Vector Maps</a>
-                              </li>
-                         </ul>
-                    </div>
-               </li>
-
                <li class="menu-title">Other</li>
 
                <li class="nav-item">
@@ -340,37 +193,6 @@
                                         <span class="nav-text">Light Mode</span>
                                         <span class="badge badge-soft-danger badge-pill text-end">Hot</span>
                                    </a>
-                              </li>
-                         </ul>
-                    </div>
-               </li>
-
-               <li class="nav-item">
-                    <a class="nav-link menu-arrow" href="#sidebarMultiLevelDemo" data-bs-toggle="collapse" role="button"
-                         aria-expanded="false" aria-controls="sidebarMultiLevelDemo">
-                         <span class="nav-icon">
-                              <iconify-icon icon="mingcute:menu-line"></iconify-icon>
-                         </span>
-                         <span class="nav-text"> Menu Item </span>
-                    </a>
-                    <div class="collapse" id="sidebarMultiLevelDemo">
-                         <ul class="nav sub-navbar-nav">
-                              <li class="sub-nav-item">
-                                   <a class="sub-nav-link" href="javascript:void(0);">Menu Item 1</a>
-                              </li>
-                              <li class="sub-nav-item">
-                                   <a class="sub-nav-link  menu-arrow" href="#sidebarItemDemoSubItem"
-                                        data-bs-toggle="collapse" role="button" aria-expanded="false"
-                                        aria-controls="sidebarItemDemoSubItem">
-                                        <span> Menu Item 2 </span>
-                                   </a>
-                                   <div class="collapse" id="sidebarItemDemoSubItem">
-                                        <ul class="nav sub-navbar-nav">
-                                             <li class="sub-nav-item">
-                                                  <a class="sub-nav-link" href="javascript:void(0);">Menu Sub item</a>
-                                             </li>
-                                        </ul>
-                                   </div>
                               </li>
                          </ul>
                     </div>
